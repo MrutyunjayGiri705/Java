f4cfa90062683b504d3241cdedfef4498f36deec
mrutyunjay <mrutyunjaygiri@gmail.com>
2025-10-25 18:59:11 +0530

    babulu

---
+++ swaptwonumber.java
@@ -0,0 +1,16 @@
+public class swaptwonumber {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        // int temp;
+        // System.out.println("a value is:"+a +"   "+"b value is:"+b);
+        // temp=a;
+        // a=b;
+        // b=temp;
+        // System.out.println("a value is:"+a +"   "+"b value is:"+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+    System.out.println("a value is:"+a +"   "+"b value is:"+b);
+    }
+}
